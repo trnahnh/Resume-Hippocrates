8974ff234d1c964398bf698ce05520e676535ec5
Anh D Tran <tran3ah@mail.uc.edu>
2025-10-11 15:02:04 -0400

    Building components, paths, and UI.

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import Banner from '../components/home/Banner'
+import Hero from '../components/home/Hero'
+import Features from '../components/home/Features'
 
 const Home = () => {
   return (
     <div>
-        <h1>Home Page</h1>
+        <Banner/>
+        <Hero/>
+        <Features/>
     </div>
   )
 }
