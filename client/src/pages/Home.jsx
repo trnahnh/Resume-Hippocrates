41ac650b7a9d42f85cfd466fb9635b3416d8ca68
Anh D Tran <tran3ah@mail.uc.edu>
2025-10-12 18:37:32 -0400

    Building more and more components, fixing UI.

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 import Banner from '../components/home/Banner'
 import Hero from '../components/home/Hero'
 import Features from '../components/home/Features'
+import Testimonial from '../components/home/Testimonial'
+import CallToAction from '../components/home/CallToAction'
+import Footer from '../components/home/Footer'
 
 const Home = () => {
   return (
@@ -9,6 +12,9 @@ const Home = () => {
         <Banner/>
         <Hero/>
         <Features/>
+        <Testimonial/>
+        <CallToAction/>
+        <Footer/>
     </div>
   )
 }
