996f4f020038c11049dca1739084b5cf672d04df
Anh D Tran <tran3ah@mail.uc.edu>
2025-10-14 00:17:02 -0400

    Progress Bar

--- client/src/pages/ResumeBuilder.jsx
+++ client/src/pages/ResumeBuilder.jsx
@@ -1,16 +1,77 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
+import { Link, useParams } from 'react-router-dom'
+import { dummyResumeData } from '../assets/assets'
+import { ArrowLeftIcon, Briefcase, FileText, FolderIcon, GraduationCap, Sparkles, User } from 'lucide-react'
 
 const ResumeBuilder = () => {
+
+  const { resumeId } = useParams()
 
   const [resumeData, setResumeData] = useState({
     _id: '',
     title: '',
-    personal_info: {}
+    personal_info: {},
+    professional_summary: "",
+    experience: [],
+    education: [],
+    project: [],
+    skills: [],
+    template: "classic",
+    accent_color: "#3B82F6",
+    public: false,
   })
+
+  const loadExistingResume = async () => {
+    const resume = dummyResumeData.find(resume => resume._id === resumeId)
+    if (resume) {
+      setResumeData(resume)
+      document.title = resume.title
+    }
+  }
+
+  const [activeSectionIndex, setActiveSectionIndex] = useState(0)
+  const [removeBackground, setRemoveBackground] = useState(false);
+
+  const sections = [
+    { id: "personal", name: "Personal Info", icon: User },
+    { id: "summary", name: "Summary", icon: FileText },
+    { id: "experience", name: "Experience", icon: Briefcase },
+    { id: "education", name: "Education", icon: GraduationCap },
+    { id: "projects", name: "Projects", icon: FolderIcon },
+    { id: "skills", name: "Skills", icon: Sparkles },
+  ]
+
+  const activeSection = sections[activeSectionIndex]
+
+  useEffect(() => {
+    loadExistingResume()
+  }, [])
 
   return (
     <div>
-        <h1>ResumeBuilder Page</h1>
+
+        <div className='max-w-7xl mx-auto px-4 py-6'>
+          <Link to={'/app'} className='inline-flex gap-2 items-center text-slate-500 hover:text-slate-700 transition-all'>
+            <ArrowLeftIcon className='size-4'/> Back to Dashboard
+          </Link>
+        </div>
+
+        <div className='max-w-7xl mx-auto px-4 pb-8'>
+          <div className='grid lg:grid-cols-12 gap-8'>
+            {/* Left Panel - Form */}
+            <div className='relative lg:col-span-5 rounded-lg overflow-hidden'>
+              <div className='bg-white rounded-lg shadow-sm border border-gray-200 p-6 pt-1'>
+                {/* Progress bar using activeSectionIndex */}
+                <hr className='absolute top-0 left-0 right-0 border-2 border-gray-200'/>
+                <hr className='absolute top-0 left-0 h-1 bg-gradient-to-r from-green-500 to-green-600 border-none transition-all duration-2000' style={{width: `${activeSectionIndex * 100 / (sections.length - 1)}%`}}/>
+              </div>
+            </div>
+
+            {/* Right Panel - Preview */}
+            <div></div>
+          </div>
+        </div>
+
     </div>
   )
 }
