05402f8437bc44bf08f82eb9dbce522834c7ea1b
Anh D Tran <tran3ah@mail.uc.edu>
2025-10-14 23:32:17 -0400

    Components

--- client/src/pages/ResumeBuilder.jsx
+++ client/src/pages/ResumeBuilder.jsx
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from 'react'
 import { Link, useParams } from 'react-router-dom'
 import { dummyResumeData } from '../assets/assets'
-import { ArrowLeftIcon, Briefcase, FileText, FolderIcon, GraduationCap, Sparkles, User } from 'lucide-react'
+import { ArrowLeftIcon, Briefcase, ChevronLeft, ChevronRight, FileText, FolderIcon, GraduationCap, Sparkles, User } from 'lucide-react'
+import PersonalInfoForm from '../components/PersonalInfoForm'
 
 const ResumeBuilder = () => {
 
@@ -64,6 +65,28 @@ const ResumeBuilder = () => {
                 {/* Progress bar using activeSectionIndex */}
                 <hr className='absolute top-0 left-0 right-0 border-2 border-gray-200'/>
                 <hr className='absolute top-0 left-0 h-1 bg-gradient-to-r from-green-500 to-green-600 border-none transition-all duration-2000' style={{width: `${activeSectionIndex * 100 / (sections.length - 1)}%`}}/>
+
+                {/* Section Navigation */}
+                <div className='flex justify-between items-center mb-6 border-b border-gray-300 py-1'>
+                  <div></div>
+                  <div className='flex items-center'>
+                    {activeSectionIndex !== 0 && (
+                      <button onClick={() => setActiveSectionIndex((prevIndex) => Math.max(prevIndex - 1, 0))} className='flex items-center gap-1 p-3 rounded-lg text-sm font-medium text-gray-600 hover:bg-gray-50 transition-all' disabled={activeSectionIndex === 0}>
+                        <ChevronLeft className='size-4'/> Previous
+                      </button>
+                    )}
+                    <button onClick={() => setActiveSectionIndex((prevIndex) => Math.min(prevIndex + 1, sections.length - 1))} className={`flex items-center gap-1 p-3 rounded-lg text-sm font-medium text-gray-600 hover:bg-gray-50 transition-all ${activeSectionIndex === sections.length - 1 && 'opacity-50'}`} disabled={activeSectionIndex === sections.length - 1}>
+                        Next <ChevronRight className='size-4'/>
+                      </button>
+                  </div>
+                </div>
+
+                {/* Form Content */}
+                <div className='space-y-6'>
+                  {activeSection.id === 'personal' && (
+                    <PersonalInfoForm data={resumeData.personal_info} onChange={(data) => setResumeData(prev => ({...prev, personal_info: data}))} removeBackground={removeBackground} setRemoveBackground={setRemoveBackground}/>
+                  )}
+                </div>
               </div>
             </div>
 
