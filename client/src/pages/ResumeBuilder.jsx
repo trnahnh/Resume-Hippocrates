df9949f58be6ac8bda6bc613977eb852e2388367
Anh D Tran <tran3ah@mail.uc.edu>
2025-10-15 23:09:18 -0400

    Building template selector.

--- client/src/pages/ResumeBuilder.jsx
+++ client/src/pages/ResumeBuilder.jsx
@@ -3,6 +3,8 @@ import { Link, useParams } from 'react-router-dom'
 import { dummyResumeData } from '../assets/assets'
 import { ArrowLeftIcon, Briefcase, ChevronLeft, ChevronRight, FileText, FolderIcon, GraduationCap, Sparkles, User } from 'lucide-react'
 import PersonalInfoForm from '../components/PersonalInfoForm'
+import ResumePreview from '../components/ResumePreview'
+import TemplateSelector from '../components/TemplateSelector'
 
 const ResumeBuilder = () => {
 
@@ -68,7 +70,11 @@ const ResumeBuilder = () => {
 
                 {/* Section Navigation */}
                 <div className='flex justify-between items-center mb-6 border-b border-gray-300 py-1'>
-                  <div></div>
+
+                  <div className='flex justify-between items-center mb-6 border-b border-gray-300 py-1'>
+                    <TemplateSelector selectedTemplate={resumeData.template} onChange={(template) => setResumeData(prev => ({...prev, template}))}/>
+                  </div>
+
                   <div className='flex items-center'>
                     {activeSectionIndex !== 0 && (
                       <button onClick={() => setActiveSectionIndex((prevIndex) => Math.max(prevIndex - 1, 0))} className='flex items-center gap-1 p-3 rounded-lg text-sm font-medium text-gray-600 hover:bg-gray-50 transition-all' disabled={activeSectionIndex === 0}>
@@ -91,12 +97,12 @@ const ResumeBuilder = () => {
             </div>
 
             {/* Right Panel - Preview */}
-            <div className='className="lg:col-span-7 max-lg:mt-6"'>
+            <div className='lg:col-span-7 max-lg:mt-6'>
               <div>
                 {/* --- button --- */}
               </div>
 
-              {/* -- resume preview */}
+              <ResumePreview data={resumeData} template={resumeData.template} accentColor={resumeData.accent_color}/>
             </div>
           </div>
         </div>
