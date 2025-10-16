a81f5535f583ce622f6cd1b59213c5bdb9560161
Anh D Tran <tran3ah@mail.uc.edu>
2025-10-16 10:50:00 -0400

    Fixing color picker map arrow function

--- client/src/pages/ResumeBuilder.jsx
+++ client/src/pages/ResumeBuilder.jsx
@@ -5,6 +5,7 @@ import { ArrowLeftIcon, Briefcase, ChevronLeft, ChevronRight, FileText, FolderIc
 import PersonalInfoForm from '../components/PersonalInfoForm'
 import ResumePreview from '../components/ResumePreview'
 import TemplateSelector from '../components/TemplateSelector'
+import ColorPicker from '../components/ColorPicker'
 
 const ResumeBuilder = () => {
 
@@ -73,6 +74,7 @@ const ResumeBuilder = () => {
 
                   <div className='flex items-center gap-2'>
                     <TemplateSelector selectedTemplate={resumeData.template} onChange={(template) => setResumeData(prev => ({...prev, template}))}/>
+                    <ColorPicker selectedColor={resumeData.accent_color} onChange={(color) => setResumeData(prev => ({...prev, accent_color : color}))}/>
                   </div>
 
                   <div className='flex items-center'>
