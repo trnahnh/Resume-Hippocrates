9536f2f9742f1fc450ee21f65b8b2cbed0fa15c2
Anh D Tran <tran3ah@mail.uc.edu>
2025-10-16 10:52:23 -0400

    Final color picker

--- client/src/components/ColorPicker.jsx
+++ client/src/components/ColorPicker.jsx
@@ -26,7 +26,7 @@ const ColorPicker = ({ selectedColor, onChange }) => {
         {isOpen && (
             <div className='grid grid-cols-4 w-60 gap-2 absolute top-full left-0 right-0 p-3 mt-2 z-10 bg-white rounded-md border border-gray-200 shadow-sm'>
                 {colors.map((color) => (
-                    <div key={color.value} className='relative cursor-pointer group flex flex-col' onClick={() => {onChange(color.value)}}>
+                    <div key={color.value} className='relative cursor-pointer group flex flex-col' onClick={() => {onChange(color.value); setIsOpen(false)}}>
                         <div className="w-12 h-12 rounded-full border-2 border-transparent group-hover:border-black/25 transition-colors" style={{backgroundColor : color.value}}>
                         </div>
                         {selectedColor === color.value && (
