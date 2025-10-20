430bda5cd69806f776faf444cc3127f2f84b7b55
Anh D Tran <tran3ah@mail.uc.edu>
2025-10-20 18:50:42 -0400

    Building skills form.

--- client/src/pages/ResumeBuilder.jsx
+++ client/src/pages/ResumeBuilder.jsx
@@ -10,6 +10,7 @@ import ProfessionalSummaryForm from '../components/ProfessionalSummaryForm'
 import ExperienceForm from '../components/ExperienceForm'
 import EducationForm from '../components/EducationForm'
 import ProjectForm from '../components/ProjectForm'
+import SkillsForm from '../components/SkillsForm'
 
 const ResumeBuilder = () => {
 
@@ -109,6 +110,9 @@ const ResumeBuilder = () => {
                   )}
                   {activeSection.id === 'projects' && (
                     <ProjectForm data={resumeData.project} onChange={(data) => setResumeData(prev => ({...prev, project : data}))}/>
+                  )}
+                  {activeSection.id === 'skills' && (
+                    <SkillsForm data={resumeData.skills} onChange={(data) => setResumeData(prev => ({...prev, skills : data}))}/>
                   )}
                 </div>
               </div>
