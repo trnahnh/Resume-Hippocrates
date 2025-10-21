7302166ac176134e512e431016a1cd8d91dba8aa
Anh D Tran <tran3ah@mail.uc.edu>
2025-10-21 00:14:34 -0400

    Preview and Loader form

--- client/src/pages/ResumeBuilder.jsx
+++ client/src/pages/ResumeBuilder.jsx
@@ -56,6 +56,25 @@ const ResumeBuilder = () => {
     loadExistingResume()
   }, [])
 
+  const changeResumeVisibility = async() => {
+    setResumeData({...resumeData, public: !resumeData.public})
+  }
+
+  const handleShare = () => {
+    const frontendUrl = window.location.href.split('/app/')[0];
+    const resumeUrl = frontendUrl + '/view/' + resumeId;
+
+    if (navigator.share) {
+      navigator.share({url: resumeUrl, text: "My Resume", })
+    } else {
+      alert('Share not supported on this browser.')
+    }
+  }
+
+  const downloadResume = () => {
+    window.print();
+  }
+
   return (
     <div>
 
@@ -127,15 +146,15 @@ const ResumeBuilder = () => {
               <div className='relative w-full'>
                 <div className='absolute bottom-3 left-0 right-0 flex items-center justify-end gap-2'>
                   {resumeData.public && (
-                    <button className='flex items-center p-2 px-4 gap-2 text-xs bg-gradient-to-br from-blue-100 to-blue-200 text-blue-600 rounded-lg ring-blue-300 hover:ring transition-colors'>
-                      <Share2Icon className='size-4'/>
+                    <button onClick={handleShare} className='flex items-center p-2 px-4 gap-2 text-xs bg-gradient-to-br from-blue-100 to-blue-200 text-blue-600 rounded-lg ring-blue-300 hover:ring transition-colors'>
+                      <Share2Icon className='size-4'/> Share
                     </button>
                   )}
-                  <button className='flex items-center p-2 px-4 gap-2 text-xs bg-gradient-to-br from-purple-100 to-purple-200 text-purple-600 ring-purple-300 rounded-lg hover:ring transition-colors'>
+                  <button onClick={changeResumeVisibility} className='flex items-center p-2 px-4 gap-2 text-xs bg-gradient-to-br from-purple-100 to-purple-200 text-purple-600 ring-purple-300 rounded-lg hover:ring transition-colors'>
                     {resumeData.public ? <EyeIcon className='size-4'/> : <EyeOffIcon className='size-4'/>}
                     {resumeData.public ? 'Public' : 'Private'}
                   </button>
-                  <button className='flex items-center gap-2 px-6 py-2 text-xs bg-gradient-to-br from-green-100 to-green-200 text-green-600 rounded-lg ring-green-300 hover:ring transition-colors'>
+                  <button onClick={downloadResume} className='flex items-center gap-2 px-6 py-2 text-xs bg-gradient-to-br from-green-100 to-green-200 text-green-600 rounded-lg ring-green-300 hover:ring transition-colors'>
                     <DownloadIcon className='size-4'/> Download
                   </button>
                 </div>
